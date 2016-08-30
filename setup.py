b63ccb9de001a4cfe2fd807fdc55550568b6d7b4
Slavik Svyrydiuk <svyrydiuk@gmail.com>
2016-08-30 08:16:05 +0200

    added download_url

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 """ Setup EasySMS """
-
 import os
-
-from setuptools import setup, find_packages
 import re
-
+from setuptools import setup, find_packages
 
 MODULE_NAME = 'easysms'
 PACKAGE_DATA = list()
@@ -23,6 +20,8 @@ _version = re.search(r'^__version__\s*=\s*"(.*)"', _meta, re.M).group(1)
 _author = re.search(r'^__author__\s*=\s*"(.*)"', _meta, re.M).group(1)
 _email = re.search(r'^__email__\s*=\s*"(.*)"', _meta, re.M).group(1)
 
+download_url = "https://github.com/sv0/easysms/archive/%s.tar.gz" % _version
+
 setup(
     name=_project,
     version=_version,
@@ -33,6 +32,7 @@ setup(
     author=_author,
     author_email=_email,
     url="https://github.com/sv0/easysms",
+    download_url=download_url,
     keywords='easysms sms heroku',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
